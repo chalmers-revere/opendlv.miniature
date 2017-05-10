32878eaef9022d37b692a4f5f3185af34d29a302
olbender <olbender@users.noreply.github.com>
2017-05-10 06:56:50 +0200

    Scale position

--- code/proxy-miniature/lps/src/Lps.cpp
+++ code/proxy-miniature/lps/src/Lps.cpp
@@ -296,6 +296,11 @@ void Lps::FindState(std::vector<opendlv::model::Cartesian3> a_needleMarkers)
 
 //  std::cout << "  yaw norm " << yaw_norm << " total yaw " << yaw_tot << " yaw mean " << yaw_mean << " final yaw " << yaw << std::endl;
 
+  // For TME290, convert to decimeters
+  x0 *= 10.0f;
+  y0 *= 10.0f;
+  z0 *= 10.0f;
+
   opendlv::model::Cartesian3 position(x0, y0, z0);
   opendlv::model::Cartesian3 angularDisplacement(roll, pitch, yaw);
   opendlv::model::State state(position, angularDisplacement, m_frameId);
